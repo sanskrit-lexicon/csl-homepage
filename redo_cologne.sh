7ad73234f553681793c76e63a942fcf2fa94ea0c
Dhaval Patel <drdhaval2785@gmail.com>
2021-09-13 13:22:15 +0530

    python3 for redo scripts

--- redo_cologne.sh
+++ redo_cologne.sh
@@ -1,4 +1,4 @@
 echo "Remaking index_cologne.html"
-python index_cologne.py indexdirs.xml index_cologne.html
+python3 index_cologne.py indexdirs.xml index_cologne.html
 echo "Copying index_cologne.html to /docs/index.html home page"
 cp index_cologne.html ../../index.html
