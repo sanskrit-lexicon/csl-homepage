17c756235c3d143b8784e79d24ef36c50d728285
Dhaval Patel <drdhaval2785@gmail.com>
2021-09-13 12:06:48 +0530

    update_version.sh update

--- update_version.sh
+++ update_version.sh
@@ -1,4 +1,3 @@
-dt=$(date '+%Y%m%d%H%M%S');
 echo "STEP 1. UPDATE THE .VERSION FILE."
 cd ../csl-orig
 echo "2.0.`git log | grep '^commit' | wc -l`" > .version
@@ -6,5 +5,3 @@ echo "2.0.`git log | grep '^commit' | wc -l`" > .version
 echo "STEP 2. UPDATE COLOGNE HOMEPAGE TO DISPLAY TODAY'S DATE."
 cd ../csl-homepage
 bash redo_xampp.sh
-cd ../csl-pywork/v02
-
